b12c950edd4eeb7dd8c3d782cf19fd4576b5813a
pavan <anonymous@anonymous.com>
2015-05-03 21:41:18 -0700

    Query the popular Media and parse it in to Photos-ArrayList.

---
+++ app/src/main/java/com/codepath/instagramviewer/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package com.codepath.instagramviewer;
+
+/**
+ * Created by pavan on 5/2/15.
+ */
+public class InstagramPhoto {
+    public String userName;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
--- app/src/main/java/com/codepath/instagramviewer/MediaDisplayerActivity.java
+++ app/src/main/java/com/codepath/instagramviewer/MediaDisplayerActivity.java
@@ -2,18 +2,72 @@ package com.codepath.instagramviewer;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class MediaDisplayerActivity extends ActionBarActivity {
+
+    public static final String CLIENT_ID = "5e4bb8b442144e2cad975512543ecdb8";
+    private ArrayList<InstagramPhoto> photos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_displayer);
+        photos = new ArrayList<>();
+        fetchPopularPhotos();
     }
 
+
+    public void fetchPopularPhotos() {
+        String url = "https://api.instagram.com/v1/media/popular?client_id="+CLIENT_ID;
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null, new JsonHttpResponseHandler() {
+
+                    public void onSuccess(int statusCode, org.apache.http.Header[] headers, JSONObject response ) {
+                        Log.i("DEBUG", response.toString());
+                        JSONArray photosJSON  = null;
+
+                        try {
+                            photosJSON = response.getJSONArray("data");
+
+                            for (int i = 0; i < photosJSON.length(); i++ ) {
+                                try {
+                                    JSONObject photoJSON = photosJSON.getJSONObject(i);
+                                    InstagramPhoto photo = new InstagramPhoto();
+                                    Log.i("DEBUG", "Got Infor For Photo: "+i);
+                                    photo.userName = photoJSON.getJSONObject("user").getString("username");
+                                    photo.caption = photoJSON.getJSONObject("caption").getString("text");
+                                    photo.imageUrl = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                                    photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                                    photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
+                                    photos.add(photo);
+                                } catch (JSONException e) {
+                                    Log.i("DEBUG", "Cannot insert Picture "+i+":"+e.toString());
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    public void onFailure(int statusCode, org.apache.http.Header[] headers, Throwable throwable) {
+                    }
+                }
+        );
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
